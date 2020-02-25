c6fa1e8abcb4768eddc258e308b3448f51547d35
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-02-25 15:19:32 -0300

    [IMP] project/install.py (ADD clv_external_sync module installation)

--- project/install.py
+++ project/install.py
@@ -198,7 +198,7 @@ class MFMng(object):
 
         # self.install_upgrade_module('clv_processing', self.upgrade_all, group_names)
 
-        # self.install_upgrade_module('clv_external_sync', self.upgrade_all, group_names)
+        self.install_upgrade_module('clv_external_sync', self.upgrade_all, group_names)
 
         # ############################################################################################
         #
