88424afc28705b5708c0ce346dee9ebf29e498a5
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-02-25 16:00:57 -0300

    [IMP] project/install.py (ADD clv_global_tag_sync_mfmng module installation)

--- project/install.py
+++ project/install.py
@@ -236,7 +236,7 @@ class MFMng(object):
 
         self.install_upgrade_module('clv_base_sync_mfmng', self.upgrade_all, group_names)
 
-        # self.install_upgrade_module('clv_global_tag_sync_mfmng', self.upgrade_all, group_names)
+        self.install_upgrade_module('clv_global_tag_sync_mfmng', self.upgrade_all, group_names)
 
         # self.install_upgrade_module('clv_mfile_sync_mfmng', self.upgrade_all, group_names)
 
